95fb84244822a561bd54213b54c093eca6a0cdc4
Brian Walker Jr <55070185+BrianJr03@users.noreply.github.com>
2023-03-07 12:35:42 -0600

    adds last updated to homepage

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "81232220e2729be7267e7becdc034939",
-"index.html": "18c0e2c13ce83c0c2652c3ced5dc887f",
-"/": "18c0e2c13ce83c0c2652c3ced5dc887f",
-"main.dart.js": "c258626fe60c5e4bb6d89c0c2572ea83",
+"index.html": "80b7568a4d47993e7d0f5dd9546bd442",
+"/": "80b7568a4d47993e7d0f5dd9546bd442",
+"main.dart.js": "f0acde823987593b5e2b37cb838824aa",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "README.md": "52ec28cd0a42ff7450ebb69600d47c24",
 "favicon.png": "87f2a64d8b91f3cd58e2fe8899574854",
